b60fbcc31b76dbf8a131880173a80f6d747f3a4c
FernRicaurte <FernRicaurte2@gmail.com>
2023-11-10 21:41:21 -0500

    refactoring the Employee object and instances of the object

--- classes.py
+++ classes.py
@@ -1,11 +1,12 @@
 class Employee:
-    def __init__(self):
-        self.name = "Ji-Soo"
-        self.age = 38
-        self.position = "developer" 
-        self.salary = 1200
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position 
+        self.salary = salary
 
-e = Employee()
-print(e.__class__)
+employee1 = Employee("Ji-Soo", 38, "developer", 1200)
+employee2 = Employee("Lauren", 44, "tester", 1000)
+print(employee1.name)
+print(employee2.name)
 
-# I have created an object with some attributes and checked for the class using __class__ as a function or method to e.
