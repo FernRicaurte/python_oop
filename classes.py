bd4e99dd8353ba2640cd86ab826bc85b2ff17565
FernRicaurte <FernRicaurte2@gmail.com>
2023-11-10 21:50:39 -0500

    adding functionality

--- classes.py
+++ classes.py
@@ -4,9 +4,16 @@ class Employee:
         self.age = age
         self.position = position 
         self.salary = salary
+        
+    def increase_salary(employee, percent):
+        employee['salary'] += employee['salary'] * (percent/100)
+        
+    def employee_info(employee):
+        print(f"{employee['name']} is {employee['age']} years old. Employee is a {employee['position']} with the salary of ${employee['salary']}")
 
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
 employee2 = Employee("Lauren", 44, "tester", 1000)
 print(employee1.name)
 print(employee2.name)
 
+#The process of using a class to create a new object is called Instantiation.
