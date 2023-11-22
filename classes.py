7d47e5ac1b8e5e07600b425fca96407ba455efc4
FernRicaurte <FernRicaurte2@gmail.com>
2023-11-22 17:48:12 -0500

    using the setter method inside the init function

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ class Employee:
         self.name = name
         self.age = age
         self.position = position 
-        self.salary = salary
+        self.set_salary = (salary)
         
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
