14f7c349a0902d94250b1b82b6a0c63b21b564d0
FernRicaurte <FernRicaurte2@gmail.com>
2023-11-22 18:44:53 -0500

    adding logic in the instnatiation for the setter method and debugged into self.set_salary(salary

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ class Employee:
         self.name = name
         self.age = age
         self.position = position 
-        self.set_salary = (salary)
+        self.set_salary(salary)
         
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
@@ -17,10 +17,35 @@ class Employee:
             f"{repr(self.name)}, {repr(self.age)}, "
             f"{repr(self.position)}, {repr(self.salary)})"
             )
-
+        
+    def get_salary(self):
+        #return f"${self.salary}"
+        #return round(self.salary, 2)
+        #logging.info("Someone accessed the salary attribute")
+        return self.get_salary
+    
+    def set_salary(self, salary):
+        if salary < 1000:
+            raise ValueError('Minimum wage is $1000')
+        self.salary = salary
+        
 employee1 = Employee("Ji-Soo", 38, "developer", 1200)
 employee2 = Employee("Lauren", 44, "tester", 1000)
-print(eval(repr(employee1)))
+
+employee1.set_salary(2000)
+print(employee1.get_salary())
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # The process of using a class to create a new object is called Instantiation.
@@ -30,3 +55,6 @@ print(eval(repr(employee1)))
 # The __str__ function is the first default built in method within the class that is retrieved, so to pass the __str__ when calling an instance, is the norm. The point of this method is to return something that the print function will be able to print, so it makes no sense to print this inside of the method too. The __str__ returns a readable representation of an object/instance.
 
 # The __repr__ function returns a more information-rich, or official, string representation of an object. This method is called by the built-in repr() function. If possible, the string returned should be a valid Python expression that can be used to recreate the object.
+
+# setter modifies the value for a private attribute. aka accessor method
+# getter retrieves the value of a private attribute. aka mutator method
